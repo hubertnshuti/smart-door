366ebdf511fa6c65a05f0abaac9abaaf84bfc29f
Hubert <72660209+hubertnshuti@users.noreply.github.com>
2026-06-14 19:18:28 +0200

    Update Infobip API configuration values

--- config.py
+++ config.py
@@ -53,10 +53,10 @@ TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "your-chat-id")
 
 # ---------- SMS (Infobip) ----------
 SMS_ENABLED = True
-INFOBIP_BASE_URL = os.environ.get("INFOBIP_BASE_URL", "qwelwq.api.infobip.com")
-INFOBIP_API_KEY  = os.environ.get("INFOBIP_API_KEY", "53fd6397ff8ca9a8b449a4d1c61a5498-84c7684c-6472-4ade-aa06-5a9ce90affb4")
+INFOBIP_BASE_URL = os.environ.get("INFOBIP_BASE_URL", "d8ypyl.api.infobip.com")
+INFOBIP_API_KEY  = os.environ.get("INFOBIP_API_KEY", "ff5f62d200256e74a517dd7f25dd5ae3-4b1e6990-9c3b-43a5-a857-ba67c6250d42")
 SMS_SENDER = "ServiceSMS"        # required test sender during free trial
-SMS_TO = os.environ.get("SMS_TO", "250780354633")   # your verified number
+SMS_TO = os.environ.get("SMS_TO", "250781497409")   # your verified number
 
 # ---------- PATHS ----------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
